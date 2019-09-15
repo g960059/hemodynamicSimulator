055c23cd07c352d44b2c78e85df82e93299a5582
Yusuke HIrakawa <g960059@gmail.com>
2019-09-15 13:12:20 +0900

    add google analytics

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Simulator from './src/pages/Simulator'
 import Landing from './src/pages/Landing'
+import ReactGA from 'react-ga';
 
 export default (props) => {
+  useEffect(() => {
+    ReactGA.initialize('UA-146532131-1');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, []);
   return (
     <Router>
       <Route path="/" exact component={Landing} />
